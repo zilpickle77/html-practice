2fd75f0f7c5c6f2982f06930e898e43ded9d4aea
zilpickle77 <zilin.s.liu@gmail.com>
2024-06-24 20:08:21 -0400

    Add files via upload

---
+++ writing stuff/Python lesson/alarm.py
@@ -0,0 +1,7 @@
+x=input("Yo how old are you: ")
+y=input("What is your name human: ")
+z=input("What month did you become alive: ")
+
+int(x)
+
+print("Hi", y, ", nice to meet you! You are", x, "years old. You were born in", z, ".")
